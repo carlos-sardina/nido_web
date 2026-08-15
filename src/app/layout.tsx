8063f19a2dda9598aaddbaa3006665c611454e09
carlos-sardina <developerAlbertoMT@gmail.com>
2026-08-15 17:49:37 -0600

    integrating next

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "../styles/index.css";
+
+export const metadata: Metadata = {
+  title: "Nido app",
+  description:
+    "Nido connects users with local childcare providers, offering a platform to find, compare, and book trusted caregivers for their children.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className="h-full m-0">{children}</body>
+    </html>
+  );
+}
